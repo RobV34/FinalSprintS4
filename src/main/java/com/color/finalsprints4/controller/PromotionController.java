344d49f2adf2068eb949848815c05a9fa004ca0d
Rob Vatcher <135687070+RobV34@users.noreply.github.com>
2024-08-01 14:44:09 -0230

    Promotion Classes Added

--- src/main/java/com/color/finalsprints4/controller/PromotionController.java
+++ src/main/java/com/color/finalsprints4/controller/PromotionController.java
@@ -1,4 +1,31 @@
 package com.color.finalsprints4.controller;
 
+import com.color.finalsprints4.model.Promotion;
+import com.color.finalsprints4.service.PromotionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/promotions")
 public class PromotionController {
+
+    private final PromotionService promotionService;
+
+    @Autowired
+    public PromotionController(PromotionService promotionService) {
+        this.promotionService = promotionService;
+    }
+
+    @GetMapping
+    public List<Promotion> getPromotions(@RequestParam String color, @RequestParam String spaceType) {
+        return promotionService.getPromotions(color, spaceType);
+    }
+
+    @PutMapping("/{id}")
+    public Promotion updatePromotion(@PathVariable Long id, @RequestBody Promotion promotion) {
+        return promotionService.updatePromotion(id, promotion);
+    }
 }
+
