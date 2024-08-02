82fdcd8f651263163ffd36e3467f6feeba3132f1
Rob Vatcher <135687070+RobV34@users.noreply.github.com>
2024-08-02 13:48:47 -0230

    Update PromotionRepository.java

--- src/main/java/com/color/finalsprints4/repository/PromotionRepository.java
+++ src/main/java/com/color/finalsprints4/repository/PromotionRepository.java
@@ -4,10 +4,8 @@ import com.color.finalsprints4.model.Promotion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface PromotionRepository extends JpaRepository<Promotion, Long> {
-    List<Promotion> findByColorAndSpaceType(String color, String spaceType);
 }
+
 
