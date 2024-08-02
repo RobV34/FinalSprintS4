c697791d4b72bc4004eb1ce25f0d42e7d76e2b46
Rob Vatcher <135687070+RobV34@users.noreply.github.com>
2024-08-02 13:48:20 -0230

    Update PromotionController.java

--- src/main/java/com/color/finalsprints4/controller/PromotionController.java
+++ src/main/java/com/color/finalsprints4/controller/PromotionController.java
@@ -3,29 +3,49 @@ package com.color.finalsprints4.controller;
 import com.color.finalsprints4.model.Promotion;
 import com.color.finalsprints4.service.PromotionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/promotions")
+@RequestMapping("/api/promotions")
 public class PromotionController {
 
-    private final PromotionService promotionService;
-
     @Autowired
-    public PromotionController(PromotionService promotionService) {
-        this.promotionService = promotionService;
-    }
+    private PromotionService promotionService;
 
     @GetMapping
-    public List<Promotion> getPromotions(@RequestParam String color, @RequestParam String spaceType) {
-        return promotionService.getPromotions(color, spaceType);
+    public List<Promotion> getAllPromotions() {
+        return promotionService.getAllPromotions();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Promotion> getPromotionById(@PathVariable Long id) {
+        return promotionService.getPromotionById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Promotion createPromotion(@RequestBody Promotion promotion) {
+        return promotionService.addPromotion(promotion);
     }
 
     @PutMapping("/{id}")
-    public Promotion updatePromotion(@PathVariable Long id, @RequestBody Promotion promotion) {
-        return promotionService.updatePromotion(id, promotion);
+    public ResponseEntity<Promotion> updatePromotion(@PathVariable Long id, @RequestBody Promotion promotionDetails) {
+        Promotion updatedPromotion = promotionService.updatePromotion(id, promotionDetails);
+        if (updatedPromotion != null) {
+            return ResponseEntity.ok(updatedPromotion);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePromotion(@PathVariable Long id) {
+        promotionService.deletePromotion(id);
+        return ResponseEntity.noContent().build();
     }
 }
+
 
