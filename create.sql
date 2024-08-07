d12a700f77d029bf0f7e97d96782676c960550a5
Amy Dalziel <amy.dalziel@keyin.com>
2024-08-07 17:33:53 -0230

    All create statements in create.sql.

--- create.sql
+++ create.sql
@@ -19,3 +19,11 @@ INSERT INTO color_DB.style (id, style_name) VALUES
 (1, 'warm'),
 (2, 'cool'),
 (3, 'neutral')
+
+INSERT INTO `color_DB`.`promotion` (`id`, `company_name`, `url`) VALUES ('1', 'Winners', 'https://www.google.ca');
+INSERT INTO `color_DB`.`promotion` (`id`, `company_name`, `url`) VALUES ('2', 'Home Hardware', 'https://www.google.ca');
+INSERT INTO `color_DB`.`promotion` (`id`, `company_name`, `url`) VALUES ('3', 'Leons', 'https://www.google.ca');
+
+UPDATE `color_DB`.`space` SET `promotion_id` = '1' WHERE (`id` = '1');
+UPDATE `color_DB`.`space` SET `promotion_id` = '2' WHERE (`id` = '2');
+UPDATE `color_DB`.`space` SET `promotion_id` = '3' WHERE (`id` = '3');
